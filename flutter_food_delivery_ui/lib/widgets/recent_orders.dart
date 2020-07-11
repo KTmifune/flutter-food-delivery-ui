44283c18ed197d51e97d72093753d1436fa43e03
Takuma Kato <k.t.mifune@gmail.com>
2020-07-11 23:02:05 +0900

    no message

--- flutter_food_delivery_ui/lib/widgets/recent_orders.dart
+++ flutter_food_delivery_ui/lib/widgets/recent_orders.dart
@@ -1,22 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/models/order.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RecentOrders extends StatelessWidget {
+  _buildRecentOrder(BuildContext context, Order order) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      width: 300.0,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(
+            color: Colors.grey,
+            width: 1.0,
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image.asset(
+                    order.food.imageUrl,
+                    height: 100.0,
+                    width: 100.0,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          order.food.name,
+                          style: GoogleFonts.roboto(
+                            textStyle: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          order.restaurant.name,
+                          style: GoogleFonts.roboto(),
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          order.date,
+                          style: GoogleFonts.roboto(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20.0),
+            width: 48.0,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: IconButton(
+              iconSize: 30.0,
+              icon: Icon(
+                Icons.add,
+                size: 30.0,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Text(
             'Recent Order',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.w600,
-              letterSpacing: 1.2,
+            style: GoogleFonts.shadowsIntoLight(
+              textStyle: TextStyle(
+                fontSize: 24.0,
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.2,
+              ),
             ),
           ),
-        )
+        ),
+        Container(
+          height: 120.0,
+          child: ListView.builder(
+            padding: EdgeInsets.only(left: 10.0),
+            scrollDirection: Axis.horizontal,
+            itemCount: currentUser.orders.length,
+            itemBuilder: (BuildContext context, int index) {
+              Order order = currentUser.orders[index];
+              return _buildRecentOrder(context, order);
+            },
+          ),
+        ),
       ],
     );
   }
